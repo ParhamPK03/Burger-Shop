8245f98e1e4dcce47e9cc2f7b7329cd5a7ddd0ff
Parham <parham.pourkhani23@gmail.com>
2024-10-21 12:20:01 +0330

    Delivery section Complete

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from './Hero/Hero'
 import Features from './Features/Features'
 import PopularBurger from './PopularBurger/PopularBurger'
+import Delivery from './Delivery/Delivery'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <Features/>
       <PopularBurger/>
+      <Delivery/>
     </div>
   )
 }
